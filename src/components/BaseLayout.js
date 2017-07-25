378b0619deaab32a25b0bf85b7f89382c9eddc9a
Craig Wendel <wendelcraig@gmail.com>
2017-07-25 17:04:34 -0500

    Add image in nav bar.

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -7,7 +7,12 @@ export default class BaseLayout extends Component {
     return (
       <div>
         <nav className='main-nav'>
-          <a href='/'>Mars Rover Images</a>
+          <div>
+            <a href='/'>Mars Rover Images</a>
+          </div>
+          <div>
+            <img src='http://lh3.googleusercontent.com/-HT0lteCRAZ4/T1aO9YYzWyI/AAAAAAAABaE/S2YBP2pdysI/s466/Mars_transparent.png' alt='' />
+          </div>
         </nav>
         <header className='header'>
           <h1>Welcome to Mars Rover Images</h1>
